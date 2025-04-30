c6b39a32efc43bcaa5892a83bfe39edb30fd295d
vibet3k <wjbryson@vibet3k.com>
2025-04-29 20:37:42 -0400

    Remove headline

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,12 +8,12 @@ const Hero = () => {
     <BackgroundLayout>
       <div className="w-full h-full flex flex-col">
         {/* Top headline */}
-        <div className="px-12 pt-12">
+        {/* <div className="px-12 pt-12">
           <div className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-white font-lexend-deca">
             <h1>Anyone can fix a broken laptop</h1>
             <h1 className="pl-3 sm:pl-8 md:pl-12 mt-2 md:mt-3">We fix broken plans</h1>
           </div>
-        </div>
+        </div> */}
         
         {/* Logo - positioned in corner */}
         <div className="absolute bottom-4 right-12">
